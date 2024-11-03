61a0218caf96c4c4845b3598342d63e73bb3be67
Antonio Amormino <crisantoamo@gmail.com>
2024-11-03 16:29:43 +0100

    transition header+StickyP.0(footer)

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,15 +1,14 @@
 const navBar = document.querySelector(".header-container");
 const btn = document.querySelector(".btn-hover");
 
-
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 550) {
-        navBar.style.background = "white";
-        btn.style.background = "#1a8917";
-    } else {
-        navBar.style.background = "#FFC017";
-        btn.style.background = "black";
-    }
+  if (window.scrollY > 550) {
+    navBar.style.background = "white";
+    btn.style.background = "#1a8917";
+  } else {
+    navBar.style.background = "#FFC017";
+    btn.style.background = "black";
+  }
 });
 
 // la transition nel csss non ho voluta metterla perchè non volevo inserire pezzi di codice non pensati completamente da me
